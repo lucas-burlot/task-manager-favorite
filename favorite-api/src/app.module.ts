0cf8e6dacb40030fb0812e6a21e10971f708236b
Pewoh <burl410@gmail.com>
2023-07-06 11:24:49 +0200

    Add GrpcReflectionModule

--- favorite-api/src/app.module.ts
+++ favorite-api/src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { FavoriteController } from './favorite/favorite.controller';
 import { FavoriteService } from './favorite/favorite.service';
 import { PrismaService } from './prisma.service';
+import { GrpcReflectionModule } from 'nestjs-grpc-reflection';
+import { grpcConfig } from './config/grpc.config';
 
 @Module({
-  imports: [],
+  imports: [GrpcReflectionModule.register(grpcConfig)],
   controllers: [AppController, FavoriteController],
   providers: [AppService, FavoriteService, PrismaService],
 })
